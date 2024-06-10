b990d9f19bcba3bedfe00dc6d1abab8070de5deb
M <woodward.michael.a@gmail.com>
2024-06-10 18:08:25 -0500

    Added basic auth and support for cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Hono } from 'hono'
 
 // Middleware
+import { cors } from 'hono/cors';
 import { logger } from 'hono/logger';
-// import { basicAuth } from 'hono/basic-auth';
+import { basicAuth } from 'hono/basic-auth';
 import { poweredBy } from 'hono/powered-by';
 import { prettyJSON } from 'hono/pretty-json';
 
@@ -13,11 +14,18 @@ const port = Bun.env.PORT || 5001;
 
 const app = new Hono()
 
+const basicAuthConfig = {
+  username: Bun.env.BASIC_AUTH_USERNAME || 'hono',
+  password: Bun.env.BASIC_AUTH_PASSWORD || 'acoolproject',
+}
+
 
 // Middleware
-app.use(poweredBy());
 app.use(logger());
+app.use(poweredBy());
+app.use(cors({ origin: '*' }));
 app.use(prettyJSON({ space: 4 }));
+app.use(basicAuth(basicAuthConfig));
 
 // Routes
 app.route('/', routes);
